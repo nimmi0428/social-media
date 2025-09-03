5983338ffe5761f15fbbbbffefee7f38097b9b02
Nimmi Singh <nimmisingh0428@gmail.com>
2025-09-04 00:14:48 +0530

    second commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
-import { use } from "react";
+
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
